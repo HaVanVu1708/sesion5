22621a16c2d1fbddeed0ad5bf6768df0ac4aa7fc
HaVanVu1708 <62437220+HaVanVu1708@users.noreply.github.com>
2020-03-21 00:24:46 +0700

    Initial commit

---
+++ codesnippet11.java
@@ -0,0 +1,24 @@
+
+package Sesson5;
+
+import java.util.Scanner;
+
+public class codesnippet11 {
+    /**
+     *@param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int cnt, number; // cnt variable is a counter variable
+
+        for (cnt = 1, number = 0; cnt <= 10; cnt++) {
+            // Scanner class is used to accept data from the keyboard
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter a number: ");
+            number = input.nextInt();
+            if (number == 0) {
+                // break statement terminates the loop
+                break;
+            } // End if statement
+        } // End of for statement
+    }
+}
---
+++ codesnippet9.java
@@ -0,0 +1,10 @@
+
+package Sesson5;
+
+public class codesnippet9 {
+    public static void main(String[] args) {
+        for( ; ; ) {
+            System.out.println("This will go on and on");
+        }
+    }
+}
